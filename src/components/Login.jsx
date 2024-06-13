10ab112c98b5bc87efbc8872c406b94269759829
sreyalakshmi <sreyalakshmisaji@gmail.com>
2024-06-13 22:47:05 +0530

    signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const[data,changedata]=useState(
+        
+        {
+           
+            "email":"",
+            "password":""
+        }
+    
+)
+const inputHandler=(event)=>{
+    changedata({...data,[event.target.name]:event.target.value},[])
+}
+const readvalue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8080/signin",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("successfully added")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch(
+            (error)=>{
+
+                console.log(error)
+            }
+        )
+    
+}
   return (
     <div>
         <div className="container">
@@ -9,14 +41,17 @@ const Login = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">email</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name="email" value={data.email} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">password</label>
-                            <input type="password" name="" id="" className="form-control" />
+                            <input type="password" id="" className="form-control" name="password" value={data.password} onChange={inputHandler}/>
                         </div><br />
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                           <button className="btn btn-success">Login</button>
+                           <button className="btn btn-success" onClick={readvalue}>Login</button>
+                        </div>
+                        <div>
+                        <a class="nav-link" href="/signup">SignUp</a>
                         </div>
                     </div>
                 </div>
